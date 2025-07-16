08dadcf37bffcb33d982cd87194b45b65cb99f7c
NathanMllt <nathan.mallet99@gmail.com>
2025-07-16 14:21:03 +0200

    Magnitude (only positive values)

--- VisCodes/Response_clustering.py
+++ VisCodes/Response_clustering.py
@@ -87,7 +87,7 @@ def process_group(group_name, mice_list):
             stim_mean[protocol].append(session_stim_mean)
             #Get the magnitude of the response (minimum or maximum in pre, stim or post periods), for normalization later
             for zneuron in zscores_concat:
-                magnitude[protocol].append(zneuron[np.argmax(np.abs(zneuron))])
+                magnitude[protocol].append(np.max(np.abs(zneuron)))
     
     #This one will be used to plot average responses per cluster (not normalized)
     single_traces = np.concatenate(single_traces[f'{protocol}'], axis=0)
