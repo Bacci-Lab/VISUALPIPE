c59d70be2f307782fe550ee2685be38882b60035
NathanMllt <nathan.mallet99@gmail.com>
2025-08-14 19:14:56 +0200

    Default parameters to analyse pyramidals

--- src/visualpipe/params/parameters_batch.py
+++ src/visualpipe/params/parameters_batch.py
@@ -1,9 +1,9 @@
-COMPILE = False
 compile_dir = ''
+COMPILE = True
 
 neuropil_impact_factor = 0.7
 F0_method = 'sliding' #sliding or hamming
-neuron_type = 'Other' #PYR or Other
+neuron_type = 'PYR' #PYR or Other
 starting_delay_2p = 0.100
 num_samples = 1000
 
@@ -12,9 +12,9 @@ facemotion_threshold = 2
 pupil_threshold = 0.5
 pupil_threshold_type = 'quantile' #std or quantile
 
-min_run_window = 1.5
-min_as_window = 1
-min_rest_window = 1
+min_run_window = 3.5
+min_as_window = 2.5
+min_rest_window = 1.5
 
 speed_filter_kernel = 5
 motion_filter_kernel = 5
