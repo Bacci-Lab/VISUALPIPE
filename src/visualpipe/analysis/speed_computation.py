Correct speed computation

src/visualpipe/analysis/speed_computation.py
@@ -35,41 +35,56 @@ def load_and_data_extraction(base_path):
     return binary_signal, radius, value_per_rotation, acq_freq
 
 def process_binary_signal(binary_signal):
-    A = binary_signal % 2
-    B = np.concatenate([A[1:], [0]])
+    # ############# DECODING TABLE ###############
+    # ## NIDAQ DIGITAL SIGNAL | 0 | 1 | 2 | 3 ####
+    # ##                   A  | 0 | 1 | 0 | 1 ####
+    # ##                   B  | 0 | 0 | 1 | 1 ####
+    # ############################################
+    A = binary_signal%2
+    B = np.floor(binary_signal/2).astype(int)
     return A, B
 
-def compute_position_from_binary_signals(A, B):
+def compute_position_from_binary_signals(A, B, forward='counterclockwise'):
     '''
     Takes traces A and B and converts it to a trace that has the same number of
     points but with positions points.
+
     Algorithm based on the schematic of cases shown in the doc
     ---------------
     Input:
         A, B - traces to convert
-
+   
     Output:
         Positions through time
 
     '''
 
-    Delta_position = np.zeros(len(A) - 1, dtype=float)  # N-1 elements
+    Delta_position = np.zeros(len(A)-1, dtype=float) # N-1 elements
+
     ################################
     ## positive_increment_cond #####
     ################################
-    PIC = ((A[:-1] == 1) & (B[:-1] == 1) & (A[1:] == 0) & (B[1:] == 1)) | \
-          ((A[:-1] == 0) & (B[:-1] == 1) & (A[1:] == 0) & (B[1:] == 0)) | \
-          ((A[:-1] == 0) & (B[:-1] == 0) & (A[1:] == 1) & (B[1:] == 0)) | \
-          ((A[:-1] == 1) & (B[:-1] == 0) & (A[1:] == 1) & (B[1:] == 1))
+    # The A signal lead the B signal (counterclockwise)
+    # ... => 11 => 01 => 00 => 10 => 11 => ...
+    PIC = ( (A[:-1]==1) & (B[:-1]==1) & (A[1:]==0) & (B[1:]==1) ) | \
+        ( (A[:-1]==0) & (B[:-1]==1) & (A[1:]==0) & (B[1:]==0) ) | \
+        ( (A[:-1]==0) & (B[:-1]==0) & (A[1:]==1) & (B[1:]==0) ) | \
+        ( (A[:-1]==1) & (B[:-1]==0) & (A[1:]==1) & (B[1:]==1) )
     Delta_position[PIC] = 1
+
     ################################
     ## negative_increment_cond #####
     ################################
-    NIC = ((A[:-1] == 1) & (B[:-1] == 1) & (A[1:] == 1) & (B[1:] == 0)) | \
-          ((A[:-1] == 1) & (B[:-1] == 0) & (A[1:] == 0) & (B[1:] == 0)) | \
-          ((A[:-1] == 0) & (B[:-1] == 0) & (A[1:] == 0) & (B[1:] == 1)) | \
-          ((A[:-1] == 0) & (B[:-1] == 1) & (A[1:] == 1) & (B[1:] == 1))
+    # The B signal lead the A signal (clockwise)
+    # ... => 11 => 10 => 00 => 01 => 11 => ...
+    NIC = ( (A[:-1]==1) & (B[:-1]==1) & (A[1:]==1) & (B[1:]==0) ) | \
+        ( (A[:-1]==1) & (B[:-1]==0) & (A[1:]==0) & (B[1:]==0) ) | \
+        ( (A[:-1]==0) & (B[:-1]==0) & (A[1:]==0) & (B[1:]==1) ) | \
+        ( (A[:-1]==0) & (B[:-1]==1) & (A[1:]==1) & (B[1:]==1) )
     Delta_position[NIC] = -1
+
+    if forward=='clockwise':
+        Delta_position = -Delta_position
 
     return np.cumsum(np.concatenate([[0], Delta_position]))
 
@@ -113,12 +128,13 @@ def resample_running_signal(original_signal,
     return new_signal
 
 def compute_speed(base_path, position_smoothing=10e-3, #s
-                  with_raw_position=False):
+                  with_raw_position=False, cpr=1000,forward='counterclockwise'):
     
-    binary_signal, radius_position_on_disk, rotoencoder_value_per_rotation, acq_freq = load_and_data_extraction(base_path)
+    binary_signal, radius_position_on_disk, _, acq_freq = load_and_data_extraction(base_path)
     A, B = process_binary_signal(binary_signal)
 
-    position = compute_position_from_binary_signals(A,B) * 2. * np.pi * radius_position_on_disk / rotoencoder_value_per_rotation
+
+    position = compute_position_from_binary_signals(A,B, forward) * 2. * np.pi * radius_position_on_disk / cpr/4.
 
     if position_smoothing > 0:
         speed = np.diff(gaussian_filter1d(position, int(position_smoothing * acq_freq), mode='nearest'))
@@ -148,10 +164,29 @@ def compute_speed(base_path, position_smoothing=10e-3, #s
         return speed, speed_time_stamps
 
 if __name__ == "__main__":
+    import sys
+    sys.path.append("./src")
     from visualpipe.analysis.ca_imaging import CaImagingDataManager
+    import visualpipe.utils.general_functions as general_functions
+    from scipy.ndimage import gaussian_filter
     
     starting_delay_2p = 0.1
-    base_path = "Y:/raw-imaging/TESTS/Mai-An/visual_test/16-00-59"
+    base_path = r"C:\Users\mai-an.nguyen\Documents\visual_stim\2025_03_19\13-52-14"
     ca_img = CaImagingDataManager(base_path, starting_delay=starting_delay_2p)
     speed, speed_time_stamps = compute_speed(base_path)
     print(speed)
+
+    speed_resampled = general_functions.resample_signal(speed, 
+                                              t_sample=speed_time_stamps, 
+                                              new_freq=ca_img.fs,
+                                              interp_time=ca_img.time_stamps,
+                                              post_smoothing=2./50.)
+
+    fig, ax = plt.subplots(2, 1, figsize=(20, 8), sharex=True)
+    ax[0].plot(ca_img.time_stamps, speed_resampled, label='resampled')
+    ax[0].plot(ca_img.time_stamps, np.abs(speed_resampled), label='speed abs resampled', linestyle='--')
+    ax[1].plot(ca_img.time_stamps, gaussian_filter(speed_resampled, 10), label='resampled filtered')
+    ax[1].plot(ca_img.time_stamps, gaussian_filter(np.abs(speed_resampled), 10), label='speed abs resampled filtered', linestyle='--')
+    ax[0].legend()
+    ax[1].legend()
+    plt.show()